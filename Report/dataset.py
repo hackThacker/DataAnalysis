7072b708c57b30dc3bae1c28736179bb6270250f
Niraj Adhikari <67623286+hackThacker@users.noreply.github.com>
2023-09-05 09:52:27 +0545

    Update dataset.py

--- Report/dataset.py
+++ Report/dataset.py
@@ -1,7 +1,7 @@
 import pandas as pd
-from pandas_profiling import ProfileReport
+from ydata_profiling import ProfileReport
 
-df = pd.read_csv('customers-100000.csv')
+df = pd.read_csv('customers-100000.csv') # enter the files name which you want to genreate a report form
 
 #generate a report
 profile = ProfileReport(df)
